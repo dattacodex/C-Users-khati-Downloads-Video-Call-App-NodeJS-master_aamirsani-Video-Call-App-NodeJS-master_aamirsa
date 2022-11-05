6df43bddb6030f91e8d06808fafbd72b7d2e3b2a
Datta Sandu Khatik <107250865+dattacodex@users.noreply.github.com>
2022-11-05 17:35:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ let stream = require( './ws/stream' );
 let path = require( 'path' );
 let favicon = require( 'serve-favicon' );
 
+const port = process.env.PORT || 4000;
+
 app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
@@ -16,4 +18,4 @@ app.get( '/', ( req, res ) => {
 
 io.of( '/stream' ).on( 'connection', stream );
 
-server.listen( 3000 );
+server.listen( port );
